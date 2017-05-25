4a43332c2bf8c0bd21118e1a37b3f4b60d8d7eed
tt <ttwang.303@gmail.com>
2017-05-25 00:20:12 -0400

    basic calculation

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var fire = new Vue({
   el: '#fire',
   data: {
-    message: 'testing vue'
+    message: 'FIRE',
+    expenses: '',
+    calc: ''
   },
   methods: {
-    changeSomething: function() {
-      this.message = 'new message'
+    calculate: function() {
+      this.calc = this.expenses * 25
     }
   }
 })
